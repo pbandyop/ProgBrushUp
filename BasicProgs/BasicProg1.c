791b60a7655c95ddec281f52a21318d13a1029f0
Payel <pbandyop@cs.helsinki.fi>
2016-03-03 23:10:39 +0530

    Basic function return

---
+++ BasicProgs/BasicProg1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*This function states that you should always return something if the function is not void*/
+
+int main(){
+	int x =10;
+	printf("The number is: %d\n", x);
+	return 0;
+}
+

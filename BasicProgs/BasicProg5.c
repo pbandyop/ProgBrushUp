3d85ba611f5ab698875319e7a272e09356381fbe
Payel <pbandyop@cs.helsinki.fi>
2016-03-04 18:17:02 +0530

    Ascii value

---
+++ BasicProgs/BasicProg5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Function to print ASCII value of characters*/
+int main(){
+
+	char x;
+	printf("Enter a character: ");
+	scanf("%c", &x);
+	printf("You entered and it's ASCII value: %d\n", x);
+	return 0;
+
+}

02c92451e363cf6bf0031f20ec15b209634457f3
Payel <pbandyop@cs.helsinki.fi>
2016-03-04 18:01:57 +0530

    Enter character

---
+++ BasicProgs/BasicProg4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Function to enter a character*/
+int main(){
+
+	char d;
+	printf("Enter a character: ");
+	scanf("%c", &d);
+	printf("You entered: %c\n", d);
+	return 0;
+
+}

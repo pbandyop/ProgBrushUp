70997dd2de4b4f978da4b320ce31dbec499fc1ab
Payel <pbandyop@cs.helsinki.fi>
2016-03-04 18:03:40 +0530

    Enter floating number

--- BasicProgs/BasicProg3.c
+++ BasicProgs/BasicProg3.c
@@ -2,8 +2,11 @@
 
 /*This program will input and print floating numbers*/
 int main(){
+
 	float num;
 	printf("Enter a floating number: ");
 	scanf("%f", &num);
 	printf("You entered %f\n", num);
+	return 0;
+
 }

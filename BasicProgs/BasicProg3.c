f4c7c7f2da1fc2721e56f06cd7b48d64e9c960f6
Payel <pbandyop@cs.helsinki.fi>
2016-03-04 17:56:36 +0530

    Float point intro

---
+++ BasicProgs/BasicProg3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*This program will input and print floating numbers*/
+int main(){
+	float num;
+	printf("Enter a floating number: ");
+	scanf("%f", &num);
+	printf("You entered %f\n", num);
+}

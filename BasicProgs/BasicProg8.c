a78ea1658990d397d25464a3e9a39704ce054ffa
Payel <pbandyop@cs.helsinki.fi>
2016-03-04 22:13:23 +0530

    Size of data types

---
+++ BasicProgs/BasicProg8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/*Function to find size of data types*/
+int main(){
+
+	int a;
+	float b;
+	double c;
+	char d;
+
+	printf("Size of int a is: %d\n", sizeof(a));
+	printf("Size of float b is: %d\n", sizeof(b));
+	printf("Size of double c is: %d\n", sizeof(c));
+	printf("Size of char d is: %d\n", sizeof(d));
+	return 0;
+
+}
+
+

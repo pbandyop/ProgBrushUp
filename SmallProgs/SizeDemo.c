f6eb1c991ff218d0920aa2edda0b3730328f4a88
Payel <pbandyop@cs.helsinki.fi>
2016-03-05 13:00:20 +0530

    Display of long

---
+++ SmallProgs/SizeDemo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*Display of long*/
+int main(){
+
+	int a;
+	long int b;
+	long long int c;
+
+	printf("Size of int a is: %d\n", sizeof(a));
+	printf("Size of long int b is: %d\n", sizeof(b));
+	printf("Size of long long int c is: %d\n", sizeof(c));
+	return 0;
+
+}
